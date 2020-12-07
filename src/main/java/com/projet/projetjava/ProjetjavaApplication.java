6656c3cfd5f1e4036b8980e715a41e7934be89f7
lo-ic <loic.beraud@le-campus-numerique.fr>
2020-12-07 11:35:35 +0100

    Installation

---
+++ src/main/java/com/projet/projetjava/ProjetjavaApplication.java
@@ -0,0 +1,13 @@
+package com.projet.projetjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetjavaApplication.class, args);
+	}
+
+}
